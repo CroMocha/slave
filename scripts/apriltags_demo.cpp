c0cf680ab4b759b041d1deb88f4456f9589dabf9
CroMocha <34674998+CroMocha@users.noreply.github.com>
2019-04-09 23:01:14 +0800

    Optimised Apriltag_demo

--- scripts/apriltags_demo.cpp
+++ scripts/apriltags_demo.cpp
@@ -165,11 +165,11 @@ public:
     m_arduino(false),
     m_timing(false),
 
-    m_width(640),
-    m_height(480),
+    m_width(320),
+    m_height(240),
     m_tagSize(0.116),
-    m_fx(432),
-    m_fy(432),
+    m_fx(260),
+    m_fy(260),
     m_px(m_width/2),
     m_py(m_height/2),
 
