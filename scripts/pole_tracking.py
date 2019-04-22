080704165c5e96e1b0e180a5bd5710ee6509f54e
CroMocha <34674998+CroMocha@users.noreply.github.com>
2019-04-22 21:11:20 +0800

    specifying pole

--- scripts/pole_tracking.py
+++ scripts/pole_tracking.py
@@ -30,76 +30,44 @@ while True:
 	# resize the frame, blur it, and convert it to the HSV
 	# color space
 	frame = imutils.resize(original_frame, width=320)
-	ratio = original_frame.shape[0] / float(frame.shape[0])
 	blurred = cv2.GaussianBlur(frame, (11, 11), 0)
 	hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
 
 	# construct a mask for the color "green", then perform
 	# a series of dilations and erosions to remove any small
 	# blobs left in the mask
-	mask = cv2.inRange(hsv, greenLower, greenUpper)
-	mask = cv2.erode(mask, None, iterations=2)
-	mask = cv2.dilate(mask, None, iterations=2)
+	pole_mask = cv2.inRange(hsv, greenLower, greenUpper)
+	pole_mask = cv2.erode(pole_mask, None, iterations=2)
+	pole_mask = cv2.dilate(pole_mask, None, iterations=2)
 
     	# find contours in the mask and initialize the current
 	# (x, y) center of the ball
-	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+	pole_cnts = cv2.findContours(pole_mask.copy(), cv2.RETR_EXTERNAL,
 		cv2.CHAIN_APPROX_SIMPLE)
-	cnts = imutils.grab_contours(cnts)
-	center = None
+	pole_cnts = imutils.grab_contours(pole_cnts)
 
 	# only proceed if at least one contour was found
-	if len(cnts) > 0:
+	if len(pole_cnts) > 0:
 		# find the largest contour in the mask, then use
 		# it to compute the minimum enclosing circle and
 		# centroid
-		c = max(cnts, key=cv2.contourArea)
-		M = cv2.moments(c)
-		center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-		#peri = cv2.arcLength(c, True)
-		#approx = cv2.approxPolyDP(c, 0.04 * peri, True)
-		#print "No. of vertices: " + str(len(approx))
-
+		c = max(pole_cnts, key=cv2.contourArea)
                 rect = cv2.minAreaRect(c)
+		pole_x = rect[0][0]
+		pole_y = rect[0][1]
+		pole_pixel_width = min(rect[1][0],rect[1][1])
+		pole_pixel_height = max(rect[1][0],rect[1][1])
+		angle_rotation = rect[2]
 		box = cv2.boxPoints(rect)
 		box = np.int0(box)
 		cv2.drawContours(frame,[box],0,(0,0,255),2)
-		x = rect[0][0]
-		y = rect[0][1]
-		width = min(rect[1][0],rect[1][1])
-		height = max(rect[1][0],rect[1][1])
-		angle_rotation = rect[2]
-		cv2.circle(frame, (int(x),int(y)), 5, (0, 255, 0), -1)
-		distance = (pole_height*focal_length/height)
-		dist_from_img_centre = x - image_width/2
-		tan_angle = 2*dist_from_img_centre*tan(0.5*fov)/image_width
-                angle = atan(tan_angle)
-		print "x: " + str(x) + "   y: " + str(y) + "   width: " + str(width) + "   height: " + str(height)
-		print "distance: " + str(distance) + "   angle: " + str(angle)
-
-		# if len(approx) == 4:
-		# 	(x, y, w, h) = cv2.boundingRect(approx)
-		# 	ar = w / float(h)
-		# 	c = c.astype("float")
-		# 	c = c.astype("int")
-		# 	cv2.drawContours(frame, [c], -1, (0, 255, 0), 1)
-		# 	print "x: " + str(x) + "   y: " + str(y) + "   width: " + str(w) + "   y: " + str(h)
-
-		# only proceed if the radius meets a minimum size
-		# if radius > 5:
-		# 	# draw the circle and centroid on the frame,
-		# 	# then update the list of tracked points
-        #     		diameter = radius*2
-		# 	dist_from_img_centre = x - image_width/2
-        #     		distance = (ball_width*focal_length/diameter)
-        #     		tan_angle = 2*dist_from_img_centre*tan(0.5*fov)/image_width
-        #     		angle = atan(tan_angle)
-		# 	print "-----------------------------------------"
-        #     		print "x: " + str(dist_from_img_centre) + "y: " + str(y) + "radius:" + str(radius)
-        #     		print "distance: " + str(distance) + "   angle: " + str(angle)
-		# 	cv2.circle(frame, (int(x), int(y)), int(radius),
-		# 		(0, 255, 255), 2)
-		# 	cv2.circle(frame, center, 5, (0, 0, 255), -1)
+		cv2.circle(frame, (int(pole_x),int(pole_y)), 3, (0, 255, 0), -1)
+		pole_distance = (pole_height*focal_length/pole_pixel_height)
+		dist_from_img_centre = pole_x - image_width/2
+		pole_tan_angle = 2*dist_from_img_centre*tan(0.5*fov)/image_width
+                pole_angle = atan(pole_tan_angle)
+		print "x: " + str(pole_x) + "   y: " + str(pole_y) + "   width: " + str(pole_pixel_width) + "   height: " + str(pole_pixel_height)
+		print "pole_distance: " + str(pole_distance) + "   pole_angle: " + str(pole_angle)
 
 	# show the frame to our screen
 	cv2.imshow("Frame", frame)
